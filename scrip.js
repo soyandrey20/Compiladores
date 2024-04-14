cc87225054fb88bd24520863eba97598a25bd847
soyAndrey <jhonyandreyburga@gmail.com>
2024-04-14 16:43:44 -0500

    Ejercicio compiladores completo

---
+++ scrip.js
@@ -0,0 +1,25 @@
+
+
+function validar() {
+    let validado = true;
+
+    let constante = document.getElementById('constante');
+    let msgConstante = document.getElementById('msgconstante');
+    let rgxDecimalPositivo = /^[0-9]*\.[0-9]+$/;
+
+    if (constante.value.trim() == '' || constante.value.length == 0) {
+
+        msgConstante.innerHTML += 'La constante es obligatoria <br>';
+        validado = false;
+
+    } if (!rgxDecimalPositivo.test(constante.value)) {
+
+        msgConstante.innerHTML += 'La constante no cumple con el formato!! (Digito.Digito)<br>';
+        validado = false;
+
+    } if (validado)
+
+        alert(' constante: ' + constante.value + ' acepatada');
+
+    return validado;
+}
